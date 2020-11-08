d496d1a7bb4601682525cf26c9967e6905d70590
Udo <udo.schmidt.jr@gmail.com>
2020-11-08 11:11:55 -0300

    Add custom Fonts/Preparee form with datePicker

--- src/pages/sprints/Create.tsx
+++ src/pages/sprints/Create.tsx
@@ -1,20 +1,49 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-
+import { Button, StyleSheet, Text } from 'react-native';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 
 export default function SprintsCreate() {
+  const [startDate, setStartDate] = useState(new Date())
+  const [show, setShow] = useState(false)
 
+  function onChangeStartDate(event: any, selectedDate?: Date) {
+    const currentDate = selectedDate || startDate;
+    if (currentDate)
+      setShow(false)
+      console.log(currentDate)
+      setStartDate(currentDate)
+  }
+
+  function showDatePicker() {
+    setShow(true)
+  }
 
   return (
-    <View>
-      <Text>Create Sprint</Text>
-    </View>
+    <ScrollView style={styles.container} contentContainerStyle={{ padding: 24 }}>
+      <Text style={styles.title}>Nova Sprint</Text>
+
+      <Text style={styles.label}>Nome</Text>
+      <TextInput style={[styles.input]} />
+
+      <Text style={styles.label}>Data Inicial</Text>
+      {/* <Button onPress={showDatepicker} title="Show date picker!" /> */}
+      { show ?
+          (
+            <DateTimePicker mode={"date"} value={startDate} onChange={onChangeStartDate}/>
+          )
+        :
+          (
+            <Button onPress={showDatePicker} title={startDate.toString()} />
+          )
+      }
+
+
+
+    </ScrollView>
   )
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -22,19 +51,29 @@ const styles = StyleSheet.create({
     backgroundColor: "#F0F2F5"
   },
   title: {
-    fontSize: 30,
+    fontFamily: "WorkSans-Black",
+    fontSize: 24,
+    color: '#2F1E21',
     textAlign: "center",
-    color: '#2F1E21'
+    paddingBottom: 24
   },
-  createSprintButton: {
-    position: 'absolute',
-    right: 24,
-    bottom: 32,
-    width: 56,
-    height: 56,
-    backgroundColor: "#1877F2",
+  label: {
+    color: '#2F1E21',
+    fontFamily: "WorkSans-Medium",
+    marginBottom:8,
+
+  },
+  input: {
+    backgroundColor: "#fff",
+    borderWidth: 1.4,
+    borderColor: '#1877F2',
+    fontFamily: "WorkSans-Medium",
     borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
+    height: 56,
+    paddingVertical: 18,
+    paddingHorizontal: 24,
+    marginBottom: 16,
+    textAlignVertical: 'top',
+
+  },
 })
